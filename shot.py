816a1102e13939408431a7022aa5fa5994d82f8d
nhat <nhat@robots.ox.ac.uk>
2024-03-18 16:09:04 +0000

    vectorised batch, 2x performance but lost me 1 hrs of mental math

--- shot.py
+++ shot.py
@@ -5,7 +5,7 @@ import torch.nn.functional as F
 from torch_geometric.transforms import BaseTransform
 from torch_geometric.utils import unbatch
 from pytorch3d.ops import estimate_pointcloud_local_coord_frames, knn_points
-from pytorch3d.structures import Pointclouds
+from pytorch3d.structures import Pointclouds, padded_to_list
 from torch_scatter import scatter_add
 
 # correction term for numerical stability
@@ -45,7 +45,6 @@ class SHOTDescriptor(nn.Module):
         curvature, lrfs = estimate_pointcloud_local_coord_frames(
             point_clouds, n_neighbors
         )  # lrfs: (B, N_max, 3, 3)
-        normals = lrfs[..., 0]  # (B, N_max, 3)
 
         # calculate neighbors
         nbh_dist2, nbh_idx, nbh = knn_points(
@@ -61,40 +60,41 @@ class SHOTDescriptor(nn.Module):
         # nbh_idx = [idx[1:] for idx in nbh_idx]
         # nbh_dist = [torch.tensor(dist[1:], dtype=torch.float) for dist in nbh_dist]
 
-        descriptors = []
-        for b in range(lrfs.shape[0]):
-            N = point_clouds_length[b]
+        # project to LRF
+        nbh_proj = nbh @ lrfs  # (B, N_max, K, 3) @ (B, N_max, 3, 3) -> (B, N_max, K, 3)
 
-            # project to LRF
-            nbh_proj = torch.bmm(
-                nbh[b, :N], lrfs[b, :N]
-            )  # (N, K, 3) @ (N, 3, 3) -> (N, K, 3)
+        upper_x = nbh_proj[..., 0] >= 0  # (B, N_max, K)
+        upper_y = nbh_proj[..., 1] >= 0  # (B, N_max, K)
+        upper_z = nbh_proj[..., 2] >= 0  # (B, N_max, K)
+        spatial_id = (upper_x << 2) + (upper_y << 1) + (upper_z)  # (B, N_max, K)
 
-            upper_x = nbh_proj[:, :, 0] >= 0  # (N, K)
-            upper_y = nbh_proj[:, :, 1] >= 0  # (N, K)
-            upper_z = nbh_proj[:, :, 2] >= 0  # (N, K)
-            spatial_id = (upper_x << 2) + (upper_y << 1) + (upper_z)
+        B, N_max, K = nbh_idx.shape
+        normals = lrfs[..., 0]  # (B, N_max, 3)
+        normals_expand = normals.unsqueeze(2).expand(B, N_max, K, 3)
+        nbh_idx_expand = nbh_idx.unsqueeze(-1).expand(B, N_max, K, 3)
+        nbh_normals = torch.gather(
+            normals_expand, 1, nbh_idx_expand
+        )  # need to gather along N_max
 
-            # nbh_idx[b, :N]: (N, K)
-            # normals[b, nbh_idx[b, :N]]: (N, K, 3)
-            cos = torch.sum(
-                normals[b, :N, None] * normals[b, nbh_idx[b, :N]], dim=-1
-            )  # (N, 1, 3) * (N, K, 3) -> (N, K, 3) -> (N, K)
-            normal_id = torch.floor(
-                self.local_bins * (cos + 1) / 2
-            )  # [-1, 1] -> [0, 1] -> [0, local_bins]
-            normal_id = torch.clamp(normal_id, 0, self.local_bins - 1)
-            bin_id = spatial_id * self.local_bins + normal_id  # (N, K)
-            descriptor = scatter_add(
-                torch.ones_like(bin_id, dtype=torch.float),
-                bin_id.type(torch.int64),
-                dim=-1,
-                dim_size=self.total_bins,
-            )  # (N, n_bins)
-            descriptors.append(descriptor)
+        cos = torch.sum(
+            normals[:, :, None] * nbh_normals, dim=-1
+        )  # (B, N, 1, 3) * (B, N_max, K, 3) -> (B, N_max, K, 3) -> (B, N, K)
 
-        shot = torch.cat(descriptors, dim=0)  # (sum(N), n_bins)
+        normal_id = torch.floor(
+            self.local_bins * (cos + 1) / 2
+        )  # [-1, 1] -> [0, 1] -> [0, local_bins]
+        normal_id = torch.clamp(normal_id, 0, self.local_bins - 1)
 
+        bin_id = spatial_id * self.local_bins + normal_id  # (B, N_max, K)
+        descriptor = scatter_add(
+            torch.ones_like(bin_id, dtype=torch.float),
+            bin_id.type(torch.int64),
+            dim=-1,
+            dim_size=self.total_bins,
+        )  # (B, N_max, n_bins)
+
+        shot = [descriptor[i, :pclen] for i, pclen in enumerate(point_clouds_length)]
+        shot = torch.cat(shot, dim=0)
         return shot if not return_lrfs else (shot, lrfs)
 
     def _compute_desc(self, points, nbh_idx, lrfs, normals):
